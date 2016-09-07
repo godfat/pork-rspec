e33dd645c94d30de8f213e9ac4baa4cdd080f73a
Lin Jen-Shin <godfat@godfat.org>
2016-09-08 04:40:49 +0800

    simpler

--- lib/pork-rspec/expect.rb
+++ lib/pork-rspec/expect.rb
@@ -3,12 +3,14 @@ require 'pork/expect'
 
 module RSpec
   class Expect < Struct.new(:pork_stat, :object)
-    def to verifier
-      verifier.call(Pork::Expect.new(pork_stat, object))
+    def to verifier, negate=false
+      expect = Pork::Expect.new(pork_stat, object)
+      expect.not if negate
+      verifier.call(expect)
     end
 
     def not_to verifier
-      verifier.call(Pork::Expect.new(pork_stat, object).not)
+      to(verifier, true)
     end
     alias_method :to_not, :not_to
   end
