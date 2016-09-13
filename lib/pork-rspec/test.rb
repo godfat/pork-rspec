6f94c3b3ac2a8d3435474cbf2a3dffbe34eea98b
Lin Jen-Shin <godfat@godfat.org>
2016-09-13 23:51:02 +0800

    so that we could also test against rspec

--- lib/pork-rspec/test.rb
+++ lib/pork-rspec/test.rb
@@ -1,5 +1,9 @@
 
-require 'pork-rspec'
+if ENV['RSPEC']
+  require 'rspec/autorun'
+else
+  require 'pork-rspec'
+end
 
 module M
   def m
@@ -20,17 +24,17 @@ class C
 end
 
 RSpec.configure do |config|
-  config.use_transactional_fixtures = false
-  config.use_instantiated_fixtures  = false
+  # config.use_transactional_fixtures = false
+  # config.use_instantiated_fixtures  = false
   config.mock_with :rspec
 
-  config.verbose_retry = true
-  config.display_try_failure_messages = true
+  # config.verbose_retry = true
+  # config.display_try_failure_messages = true
 
   config.include M, type: :controller
   config.include M::N
 
-  config.infer_spec_type_from_file_location!
+  # config.infer_spec_type_from_file_location!
   config.raise_errors_for_deprecations!
 
   config.before(:suite) do
